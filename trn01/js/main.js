7709fee9adbad92377054da3ec416cb9795cc5b1
Openmindsmile <104567031+Openmindsmile@users.noreply.github.com>
2022-05-04 14:00:25 +0900

    Update main.js

--- trn01/js/main.js
+++ trn01/js/main.js
@@ -1,14 +1,22 @@
 // window.addEventListener('DOMcontentloaded', function) {
 // document.querySelector('.header').style.display = 'none';
 // })
-
+// 객체들은 끝에 콤마를 붙인다 - 세미콜론이 아님 
 $(function () {
     $('.main_slider').slick({
         arrows: false,
         autoplay: true,
-        autoplaySpeed: 900,
+        autoplaySpeed: 1000,
         vertical: true,
         dots: true,
-    });
+    })
+
+
+    $('.mopen').on('click', function () {
+        $('.gnb').toggleClass('on')
+    })
 
 })
+
+// 객체들은 끝에 콤마를 붙인다 - 세미콜론이 아님
+// 슬라이드 텍스트에 그라데이션 넣기 과제
